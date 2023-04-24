c322a5d5569870a25200f5cb438381e5b128aab5
Emakiflom <amankflom23@gmail.com>
2023-04-24 05:06:58 -0700

    get_flag.c

--- main.h
+++ main.h
@@ -2,13 +2,38 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * struct flags - struct containing flags to "turn on".
+ * @plus: flag for the '+' character.
+ * @space: flag for the ' ' character.
+ * @hash: flag for the '#' character.
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/**
+ * struct printHandler - struct to choose the right function depending
+ * @c: format specifier.
+ * @f: pointer to the correct printing function.
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} print_h;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list c);
 int print_string(va_list s);
-int print_int(va_list i);
-int print_dec(va_list d);
+int print_int(va_list i, flags_t *f);
+int print_dec(va_list d, flags_t *f);
 
 /**
   * struct code_format - Struct format
