3c82f296e3261a54577947f45fed88ff5dc09dcc
Emakiflom <amankflom23@gmail.com>
2023-04-24 05:22:03 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -27,6 +27,35 @@ int (*find_function(const char *format))(va_list)
 }
 
 /**
+ * get_flag - turns on flags if _printf finds
+ * @s: character that holds the flag specifier
+ * @f: pointer to the struct flags in which we turn the flags on
+ * Return: 1 if a flag has been turned on, 0 otherwise
+ */
+
+int get_flag(char s, flags_t *f)
+{
+	int i = 0;
+
+	switch (s)
+	{
+		case '+':
+			f->plus = 1;
+			i = 1;
+			break;
+		case ' ':
+			f->space = 1;
+			i = 1;
+			break;
+		case '#':
+			f->hash = 1;
+			i = 1;
+			break;
+	}
+	return (i);
+}
+
+/**
   * _printf - function produces output according to a format.
   * @format: format determine format type
   * Return: size output text;
@@ -37,10 +66,15 @@ int _printf(const char *format, ...)
 	va_list af;
 	int (*f)(va_list);
 	unsigned int i = 0, cpint = 0;
+	const char *p;
+
+	p = format;
 
 	if (format == NULL)
 		return (-1);
 	va_start(af, format);
+	flags_t flags = {0, 0, 0};
+
 	while (format[i])
 	{
 		while (format[i] != '%' && format[i])
@@ -58,6 +92,12 @@ int _printf(const char *format, ...)
 			i += 2;
 			continue;
 		}
+		while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
 		if (!format[i + 1])
 			return (-1);
 		_putchar(format[i]);
