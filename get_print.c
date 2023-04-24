507a68a1e89bf32d68d354e2d8b7e379af807c54
Emakiflom <amankflom23@gmail.com>
2023-04-24 16:38:44 -0700

    get_print.c

---
+++ get_print.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * get_print - selects the right printing function to use.
+ * @s: character to cach the flag specifier.
+ * func_arr[] to find a match b/w the specifier that passed to _printf.
+ * Return: a pointer maching the pointer.
+ */
+int (*get_print(char s))(va_list, flags_t *)
+{
+	ph func_arr[] = {
+		{'i', print_int},
+		{'s', print_string},
+		{'c', print_char},
+		{'d', print_int},
+		{'u', print_unsigned},
+		{'x', print_hex},
+		{'X', print_hex_big},
+		{'b', print_binary},
+		{'o', print_octal},
+		{'R', print_rot13},
+		{'r', print_rev},
+		{'S', print_bigS},
+		{'p', print_address},
+		{'%', print_percent}
+		};
+	int flags = 14;
+
+	register int i;
+
+	for (i = 0; i < flags; i++)
+		if (func_arr[i].c == s)
+			return (func_arr[i].f);
+	return (NULL);
+}
